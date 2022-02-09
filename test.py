f202464d6fca71019e94b2a1bc7547dbb77b9466
Jacob Walls <jacobtylerwalls@gmail.com>
2022-02-09 10:40:29 -0500

    include test script

---
+++ test.py
@@ -0,0 +1,9 @@
+with open('venv2/test.py', 'w+') as f:
+  f.write('import distutils.util')
+
+from pylint.lint import Run
+
+Run(["test.py", "--disable=all", "--enable=unused-import"])
+
+import os
+os.remove('venv2/test.py')
